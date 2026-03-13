f49f2a6ce4a9422c46e40b5ce74e2d3e57564bec
Jet-Lu888 <jet.lu@stu.ocsb.ca>
2026-03-12 20:51:38 -0400

    Created the program

--- main.ts
+++ main.ts
@@ -1,8 +1,86 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Jet Lu
+ * Created on: Mar 2026
+ * This program will detect light.
 */
 
-basic.showString('Hello, World!')
+// variables
+let neopixelStrip: neopixel.Strip = null
+let variableLight: number = 50
+
+// setup
+basic.clearScreen()
+neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.show()
+basic.showIcon(IconNames.Happy)
+
+// program
+input.onButtonPressed(Button.A, function (){
+  basic.clearScreen()
+  variableLight = input.lightLevel()
+  basic.showNumber(variableLight)
+
+  // lower than 51
+  if (variableLight <= 51) {
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+    basic.showIcon(IconNames.Yes)
+    neopixelStrip.show()
+  }
+
+  // greater than 52 lower than 104
+  if (variableLight >= 52 && variableLight <= 104) {
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.White))
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+    basic.showIcon(IconNames.Yes)
+    neopixelStrip.show()
+  }
+
+  // greater than 104
+  if (variableLight > 104 && variableLight <= 156) {
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.White))
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.White))
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+    basic.showIcon(IconNames.Yes)
+    neopixelStrip.show()
+  }
+
+  // greater than 156
+    if (variableLight > 156 && variableLight <= 208) {
+     neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.White))
+     neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.White))
+     neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.White))
+     neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+     basic.showIcon(IconNames.Yes)
+     neopixelStrip.show()
+  }
+
+  // greater than 208
+  if (variableLight > 208) {
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.White))
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.White))
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.White))
+    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.White))
+    basic.showIcon(IconNames.Yes)
+    neopixelStrip.show()
+  }
+
+  // reset
+  pause(5000)
+  basic.showIcon(IconNames.Happy)
+  neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+  neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+  neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+  neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+  neopixelStrip.show()
+})
